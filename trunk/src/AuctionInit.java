4c7b64321c0347c3d09826887d1ccfc5f1b2961b
SantiagoCasablanca@gmail.com <SantiagoCasablanca@gmail.com@c8ee21ec-0a49-edef-6a25-1516282f0a25>
2011-05-17 19:00:15 +0000

    Just started: AuctionInit.java; VickreyNegotiationFacilitator.class;
    TO DO:
    - Issues

--- trunk/src/AuctionInit.java
+++ trunk/src/AuctionInit.java
@@ -1,23 +1,31 @@
+import antlr.collections.impl.Vector;
+import ei.onto.negotiation.Negotiate;
+import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
+import jade.lang.acl.ACLMessage;
+import jade.proto.ContractNetInitiator;
 
 
-public class AuctionInit extends Behaviour {
+public class AuctionInit extends ContractNetInitiator {
 
-	@Override
-	public void action() {
-		//search the DF for potential buyers
-		//start the auction by sending an appropriate initial message
+	
+	public AuctionInit(Agent a, ACLMessage cfp, Negotiate negotiate) {
+		super(a, new ACLMessage(ACLMessage.CFP));
 		
-		
-		// TODO Auto-generated method stub
-
 	}
-
-	@Override
-	public boolean done() {
-		// end
-		// TODO Auto-generated method stub
-		return false;
+	
+	
+	//prepareCFP -> Deals with the goods and stuff
+	/**
+    This method must return the vector of ACLMessage objects to be sent. 
+	It is called in the first state of this protocol. 
+	This default implementation just returns the ACLMessage object (a CFP) passed in the constructor. 
+	Programmers might prefer to override this method in order to return a vector of CFP objects 
+	for 1:N conversations or also to prepare the messages during the execution of the behaviour. 
+	 */
+	protected java.util.Vector prepareCfps(ACLMessage cfp){
+		return null;
+		
 	}
 
 }
--- trunk/src/VickerySeller.java
+++ trunk/src/VickerySeller.java
@@ -1,7 +1,41 @@
+import ei.onto.negotiation.Negotiate;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
 
-public class VickerySeller extends ei.agent.ExternalAgent{
 
+@SuppressWarnings("serial")
+public class VickerySeller extends ei.agent.ExternalAgent {
+
+	public void setup(){
+		super.setup();
+		
+		ServiceDescription sd = new ServiceDescription();
+		sd.setName("seller");
+		sd.setType("");
+		super.regService(sd);
+		
+	}
 	
+	public boolean sendRequest(ACLMessage request, Negotiate negotiate){
+		
+		DFAgentDescription template = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("Vickery");
+		template.addServices(sd);
+		/*
+		try {
+			DFAgentDescription[] results = DFService.search(this, template);
+			for(int i=0; i<results.length;++i){
+				
+			}
+		} catch(){
+			
+		}*/
+		
+		return false;
+	}
 	
 	@Override
 	protected boolean createGUI() {
