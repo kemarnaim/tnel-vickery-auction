c82335ad68e514e3cae9046924b0681b70e5263f
alexander.lazarov <alexander.lazarov@gmail.com>
2011-05-24 18:27:43 +0000

    added the agents to the ei-config.xml
    aaa tooo lazy to exlpain. we know what we did :P

--- src/AuctionInit.java
+++ src/AuctionInit.java
@@ -1,19 +1,23 @@
-import antlr.collections.impl.Vector;
+
 import ei.onto.negotiation.Negotiate;
 import jade.core.Agent;
-import jade.core.behaviours.Behaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 import jade.proto.ContractNetInitiator;
-
+import java.util.Vector;
 
 public class AuctionInit extends ContractNetInitiator {
+	private ACLMessage request;
 
-	
-	public AuctionInit(Agent a, ACLMessage cfp, Negotiate negotiate) {
+
+	public AuctionInit(Agent a, ACLMessage request, Negotiate negotiate) {
 		super(a, new ACLMessage(ACLMessage.CFP));
 		
+		this.request = request;
 	}
-	
 	
 	//prepareCFP -> Deals with the goods and stuff
 	/**
@@ -23,9 +27,67 @@ public class AuctionInit extends ContractNetInitiator {
 	Programmers might prefer to override this method in order to return a vector of CFP objects 
 	for 1:N conversations or also to prepare the messages during the execution of the behaviour. 
 	 */
-	protected java.util.Vector prepareCfps(ACLMessage cfp){
-		return null;
+	protected Vector<ACLMessage> prepareCfps(ACLMessage cfp)
+	{
+		// get the description of the Auctioneer
+		DFAgentDescription[] template = searchDF(cfp);
+		// prepare request
 		
+		Vector<ACLMessage> result = new Vector<ACLMessage>(); // prep result vector
+		cfp.setContent("Do you want to buy a car?");
+		for (int i = 0; i < template.length; i++)
+		{
+			cfp.addReceiver(template[i].getName());
+		}
+		result.add(cfp);
+		return result;
+	}
+	//This method is called when all the responses have been collected or when the timeout is expired.
+	@Override
+	protected void handleAllResponses(Vector responses, Vector acceptances) 
+	{
+		if(responses.size() < 2){
+			//failure
+		} else {
+			for (int i = 0; i < responses.size(); i++)
+			{
+				responses.get(i);
+				if (responses.get(i))
+				{
+					
+				}
+			}
+		}
+	}
+	
+	
+	
+	private DFAgentDescription[] searchDF(ACLMessage request) {
+		DFAgentDescription template = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("Bidder");
+		template.addServices(sd);
+		DFAgentDescription[] results = null;
+		
+		try {
+			results = DFService.search(myAgent, template);
+			
+		} catch(FIPAException fe){
+			fe.printStackTrace();
+		}
+		
+		return results;
 	}
 
+	public int onEnd()
+	{
+		ACLMessage reply = request.createReply();
+		reply.setContent("the winer is:");
+		reply.setPerformative(ACLMessage.INFORM); // depending if it is failure or ...
+		myAgent.send(reply);
+		
+		return 0;
+		
+
+	}
 }
