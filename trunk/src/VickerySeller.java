d04ee7348fe0c514e9337ceab283634c4cf9bf62
alexander.lazarov@gmail.com <alexander.lazarov@gmail.com@c8ee21ec-0a49-edef-6a25-1516282f0a25>
2011-05-24 18:27:43 +0000

    added the agents to the ei-config.xml
    aaa tooo lazy to exlpain. we know what we did :P

--- trunk/src/VickerySeller.java
+++ trunk/src/VickerySeller.java
@@ -1,5 +1,6 @@
 import ei.onto.negotiation.Negotiate;
 import jade.domain.DFService;
+import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
@@ -12,35 +13,50 @@ public class VickerySeller extends ei.agent.ExternalAgent {
 		super.setup();
 		
 		ServiceDescription sd = new ServiceDescription();
-		sd.setName("seller");
+		sd.setName("Seller");
 		sd.setType("");
-		super.regService(sd);
-		
+		super.regService(sd);		
 	}
 	
 	public boolean sendRequest(ACLMessage request, Negotiate negotiate){
 		
-		DFAgentDescription template = new DFAgentDescription();
-		ServiceDescription sd = new ServiceDescription();
-		sd.setType("Vickery");
-		template.addServices(sd);
-		/*
-		try {
-			DFAgentDescription[] results = DFService.search(this, template);
-			for(int i=0; i<results.length;++i){
-				
-			}
-		} catch(){
+		DFAgentDescription[] template = searchDF(request, negotiate); // get the description of the Auctioneer
+		ACLMessage msg = new ACLMessage(ACLMessage.REQUEST); // prepare request
+		msg.addReceiver(template[0].getName()); // add receiver to the new message
+		msg.setContent("I am sellings 5 cars for now");
 			
-		}*/
-		
+		send(msg); // send the message
+			
 		return false;
 	}
 	
+	private DFAgentDescription[] searchDF(ACLMessage request, Negotiate negotiate) {
+		DFAgentDescription template = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("Auctioneer");
+		template.addServices(sd);
+		DFAgentDescription[] results = null;
+		
+		try {
+			results = DFService.search(this, template);
+			
+		} catch(FIPAException fe){
+			fe.printStackTrace();
+		}
+		
+		return results;
+	}
+	
+	
 	@Override
 	protected boolean createGUI() {
-		// TODO Auto-generated method stub
+		// on click button
+		// create itselft and 
+		
+		// send request on button click
 		return false;
 	}
+	
+	
 
 }
